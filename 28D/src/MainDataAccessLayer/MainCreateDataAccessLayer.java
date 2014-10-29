f579f818def9fe95afb6594d502b914f87139d1f
Bumbibear <markusxx93@gmail.com>
2014-10-29 12:14:43 +0100

    Initial methods

--- 28D/src/MainDataAccessLayer/MainCreateDataAccessLayer.java
+++ 28D/src/MainDataAccessLayer/MainCreateDataAccessLayer.java
@@ -1,5 +1,41 @@
 package MainDataAccessLayer;
 
-public class MainCreateDataAccessLayer {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
 
+import MainModel.*;
+
+public class MainCreateDataAccessLayer {
+	Connection connect;
+	private String sqlString;
+	private boolean exception = false;
+	
+	public void registerStudent(MainStudent Student){
+		
+	}
+	
+	public void registerCourse(MainCourse Course){
+		
+	}
+	
+	public void registerStudentForCourse(MainStudent Student){
+		
+	}
+	
+	public void deleteStudent(MainStudent Student){
+		
+	}
+	public void deleteCourse(MainCourse Course){
+		
+	}
+	
+	public void deleteStudentReadingCourse(MainCourse Course, MainStudent Student){
+		
+	}
+
+	public void registerStudentGrade(MainStudent Student, MainCourse Course){
+		
+	}
 }
