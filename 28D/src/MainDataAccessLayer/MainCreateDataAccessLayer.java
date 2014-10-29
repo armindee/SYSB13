6b4bc800054785b9b85b2507478905a7928d577f
armindee <armin_@live.com>
2014-10-29 12:26:17 +0100

    Update MainCreateDataAccessLayer.java

--- 28D/src/MainDataAccessLayer/MainCreateDataAccessLayer.java
+++ 28D/src/MainDataAccessLayer/MainCreateDataAccessLayer.java
@@ -13,30 +13,31 @@ public class MainCreateDataAccessLayer {
 	private boolean exception = false;
 	
 	public void registerStudent(MainStudent Student){
-		
-		
+		connection(createQueryForStudent("student", student));
 	}
 	
 	public void registerCourse(MainCourse Course){
-		
+		connection(createQueryForCourse("Course", course));
 	}
 	
 	public void registerStudentForCourse(MainStudent Student){
-		
+		connection(createQueryForStudentReadingCourse("Registered" , student, course));
 	}
 	
 	public void deleteStudent(MainStudent Student){
-		
+		connection(createQueryForDeletingStudent("student", student));
 	}
 	public void deleteCourse(MainCourse Course){
-		
+		connection(createQueryForDeletingCourse("Course", course));
 	}
 	
 	public void deleteStudentReadingCourse(MainCourse Course, MainStudent Student){
-		
+		connection(createQueryForDeletingStudentFromCourse("Registered", course, student));
 	}
 
 	public void registerStudentGrade(MainStudent Student, MainCourse Course){
-		
+		connection(createQueryForGrading("Result", student, course, grade));
 	}
+
+	
 }
